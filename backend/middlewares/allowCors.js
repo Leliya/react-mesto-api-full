22e29ae1854088abbe74e0385642309a1642314d
Leliya <98910653+Leliya@users.noreply.github.com>
2022-10-05 15:01:29 +0400

    bugfix:credentials in all request

--- backend/middlewares/allowCors.js
+++ backend/middlewares/allowCors.js
@@ -7,10 +7,10 @@ module.exports.allowCors = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Headers', requestHeaders);
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Credentials', 'true');
     }
     return res.end();
   }
