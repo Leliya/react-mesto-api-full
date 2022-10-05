ec20e845f3e5e2cfcfc0583796444103f376b912
Leliya <98910653+Leliya@users.noreply.github.com>
2022-10-05 14:49:40 +0400

    bugfix:еще одно изменение в allowcors

--- backend/middlewares/allowCors.js
+++ backend/middlewares/allowCors.js
@@ -11,8 +11,8 @@ module.exports.allowCors = (req, res, next) => {
       res.header('Access-Control-Allow-Headers', requestHeaders);
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       res.header('Access-Control-Allow-Credentials', 'true');
-      return res.end();
     }
+    return res.end();
   }
   return next();
 };
